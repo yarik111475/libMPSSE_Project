5a7a6c23d83ed6954cdb20a280155450f539bed3
yarik111475 <yarik111475@mail.ru>
2018-09-09 16:20:32 +0400

    Commited 09.09.2018 at 16:20

--- libMPSSE_Project/Program.cs
+++ libMPSSE_Project/Program.cs
@@ -7,19 +7,24 @@ using System.IO;
 namespace libMPSSE_Project {
     class Program {
         static void Main(string[] args) {
-            I2C i2c = new I2C();
             FT_STATUS status = FT_STATUS.FT_OK;
+            SPI spi = new SPI();
+            status |= spi.Init(0);
+            status |= spi.Close();
+
+            I2C i2c = new I2C();
+            
             status |= i2c.Init(0);
 
             //FT_DEVICE_LIST_INFO_NODE listInfo = new FT_DEVICE_LIST_INFO_NODE();
             //status |= I2C.I2C_GetChannelInfo(0, ref listInfo);
 
             byte[] inBuffer = new byte[0x100];// File.ReadAllBytes(@"C:\BWD 129 (ALL)_283321400510_6401.bin");
-            //Console.WriteLine("writing...");
+            Console.WriteLine("writing...");
 
-            //for (int i = 0; i < inBuffer.Length; i++) {
-            //    status |= i2c.WriteByte(0x50, (uint)i, 0xAD);
-            //}
+            for (int i = 0; i < inBuffer.Length; i++) {
+                status |= i2c.WriteByte(0x50, (uint)i, 0x01);
+            }
 
             Console.WriteLine("reading...");
             byte[] outBuffer = new byte[inBuffer.Length];
